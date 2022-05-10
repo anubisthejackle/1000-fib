3c9ce6f91253ca2b712806fdd8003607ea746c7c
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-05-10 10:19:05 -0400

    Scaffold empty function to make test pass

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ namespace Thousand_Fib;
 function get_fibonacci_index_by_length( int $length ): int {
 	return ( $length == 2 ) ? 7 : 12;
 }
+
+function get_fibonacci_number_by_length( int $length ): int {
+
+}

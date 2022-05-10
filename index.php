42810f85a02f2ebc33b6394667ee34d666ce2f81
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-05-10 09:34:49 -0400

    Remove files that we don't need

--- index.php
+++ index.php
@@ -1,24 +1,2 @@
 <?php
-/**
- * Reusable extensions for the Thousand Fib site.
- *
- * Plugin Name: Thousand Fib Extensions
- * Plugin URI: https://github.com/alleyinteractive/thousand-fib
- * Description: Extensions to the Thousand Fib site.
- * Version: 1.0.0
- * Author: Alley
- *
- * @package Thousand_Fib
- */
-
 namespace Thousand_Fib;
-
-// Include functions for working with assets (primarily JavaScript).
-require_once __DIR__ . '/inc/assets.php';
-require_once __DIR__ . '/inc/asset-loader-bridge.php';
-
-// Include functions for working with meta.
-require_once __DIR__ . '/inc/meta.php';
-
-// Include functions.php for registering custom post types, etc.
-require_once __DIR__ . '/functions.php';
--- slotfills/index.js
+++
@@ -1,6 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-
-// Sections.
-import OpenGraph from './sections/open-graph';
-
-registerPlugin('thousand-fib-open-graph', { render: OpenGraph });

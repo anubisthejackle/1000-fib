b5a356f688cf1896eceb8a90d472ea8f7a067eeb
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-05-19 10:17:13 -0400

    Make test pass

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 namespace Thousand_Fib;
 
 function get_fibonacci_index_by_length( int $length ): int {
-	return ( $length == 2 ) ? 7 : 12;
+	$last_fib = 1;
+	$fib      = 1;
+	$index    = 2;
+	while( strlen( $fib ) < $length ){
+		$new_last_fib = $fib;
+		$fib = $last_fib + $new_last_fib;
+		$last_fib = $new_last_fib;
+		$index++;
+	}
+
+	return $index;
 }
